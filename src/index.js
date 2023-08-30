4c33bbbdcad18d2e89df537f7ad507871764c14f
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-08-30 19:02:29 +0300

    I made a  fetch request on The Cat API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import Notiflix from 'notiflix';
 import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = 'live_iNW5yRLQPuv27Qr1jI2atPHfmiX6QreVO2mkLHdj85XCpEeqizfV0zZCMXPsrFAk'
+// axios.defaults.headers.common["x-api-key"] = 'live_iNW5yRLQPuv27Qr1jI2atPHfmiX6QreVO2mkLHdj85XCpEeqizfV0zZCMXPsrFAk'
 
 
-// const keyApi = {
-//    'x-api-key' : 'live_iNW5yRLQPuv27Qr1jI2atPHfmiX6QreVO2mkLHdj85XCpEeqizfV0zZCMXPsrFAk' ,
-// };
+const keyApi = {
+  headers: {
+    'x-api-key' : 'live_iNW5yRLQPuv27Qr1jI2atPHfmiX6QreVO2mkLHdj85XCpEeqizfV0zZCMXPsrFAk',
+  }
+};
 
-const url = 'https://api.thecatapi.com/v1/breeds';
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+
 let storedBreeds = [];
+// breeds
 
 const refs ={
   select: document.querySelector('.breed-select'),
@@ -17,3 +21,30 @@ const refs ={
   loader: document.querySelector('.loader'),
   error:  document.querySelector('.error'),
 }
+
+function fetchBreeds() {
+
+  return fetch(BASE_URL,keyApi)
+  .then((response) => {
+    if(!response.ok){
+      throw new Error(response.statusText)
+    }
+    return response.json();
+  })
+  
+}
+fetchBreeds().then((data) => {
+  data = data.filter(img=> img.image?.url!=null)
+   
+  storedBreeds = data;
+  console.log(storedBreeds);
+  
+  // for (let i=0; storedBreeds.length; i ++){
+  //   const breed = storedBreeds[i];
+  //   // console.log(i);
+  // }
+})
+.catch(err=> console.log(err))
+// fetch(BASE_URL,keyApi)
+//   .then((response) => response.json ).then(console.log)
+  
